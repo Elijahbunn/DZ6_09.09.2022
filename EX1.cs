8d4042c33d68f538a45013038d0cebf12dd4606f
Elijah <eligahbunn@gmail.com>
2022-09-09 22:53:33 +0300

    coomit

--- EX1.cs
+++ EX1.cs
@@ -12,8 +12,14 @@
 
 void PrintArray(double[,] array)
 {
-    foreach (double element in array)
-        Console.WriteLine($"{element} ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"| {array[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
 }
 
 double[,] massive = new double[5, 3];
