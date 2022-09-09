8f6f1694f31766dbcda2aab4f5910b892351c775
Elijah <eligahbunn@gmail.com>
2022-09-09 22:47:56 +0300

    coomit

---
+++ EX1.cs
@@ -0,0 +1,21 @@
+﻿void FillArray(double[,] array, int minimum, int maximum)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.NextDouble() * rand.Next(minimum, maximum);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    foreach (double element in array)
+        Console.WriteLine($"{element} ");
+}
+
+double[,] massive = new double[5, 3];
+FillArray(massive, 1, 100);
+PrintArray(massive);
